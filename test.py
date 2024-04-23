fbe0b40c59e6e9f2c2f224c4674f61a65e089db6
dungmihoyo <165217661+dungmihoyo@users.noreply.github.com>
2024-04-23 17:32:39 +0700

    Update_test.py

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 import tkinter as tk 
+import sqlite3
+
+
 
 # Khoi tao cua so dang nhap 
 window_login = tk.Tk()
@@ -33,26 +36,6 @@ pass_word_task.place(x = 60, y = 100)
 text_user_name.place(x = 150, y = 60) 
 text_pass_word.place(x = 150, y = 100) 
 
-# ham duoc goi sau khi na nut login 
-def Check_PassWord( ) : 
-    text = tk.StringVar()
-
-    for i in dict : 
-        if(user_name.get() == i): 
-            if( pass_word.get() == dict[i]): 
-                text = tk.StringVar(window_login, "Sucessfully !!! ")
-            else : 
-                text = tk.StringVar(window_login, "Wrong pass or user_name !!! ")
-        
-    message = tk.Label(window_login, textvariable= text)
-    message.pack()
-   
-# neu nguoi dung chua co tai khoan, tao ham giup nguoi dung nhap vao thong tin ca nhan 
-def Add_Dict( ) : 
-    dict[new_user.get()] = new_pass.get()
-    for i in dict : 
-        print (f"user name: {i} | pass word: {dict[i]}")
-    
 
 def Add_infor ( ) : 
     # Dung cua so cu va tao cua so moi 
@@ -63,7 +46,7 @@ def Add_infor ( ) :
     #Tao label cho nguoi dung nhap vao ten 
     new_user_name = tk.Label(add, text = "New User Name :")
     new_pass_word = tk.Label(add, text = "New Pass Word :")
-
+    
     # Tao text bar cho nguoi dung nhap thong tin ca nhan 
     new_user_name_text = tk.Entry(add, textvariable=new_user)
     new_pass_word_text = tk.Entry(add, textvariable=new_pass) 
@@ -75,14 +58,30 @@ def Add_infor ( ) :
     new_user_name_text.place(x = 150, y = 60) 
     new_pass_word_text.place(x = 150, y = 100) 
     
-    add_new_button = tk.Button(add, text="Create new", command=Add_Dict)
+    add_new_button = tk.Button(add, text="Create new", command=lambda: add.destroy())
+    user_n = new_user.get()
+    pass_w = new_pass.get()
+    dict[user_n] = pass_w
+    new_user.set("")
+    new_pass.set("")
     add_new_button.place(x = 150, y = 150 )
     
-    
-# tao ra nut giup nguoi dung dang nhap 
-login_button = tk.Button(window_login, text = "login", command= Check_PassWord) 
-login_button.place(x = 200, y = 150) 
 
+
+# ham duoc goi sau khi na nut login 
+def Check_PassWord( ) : 
+    flag = False
+    for i in dict : 
+        if(user_name.get() == i): 
+            if( pass_word.get() == dict[i]): 
+                window_login.destroy()
+                
+    window_login.mainloop() 
+        
+# tao ra nut giup nguoi dung dang nhap 
+
+login_button = tk.Button(window_login, text = "login", command=Check_PassWord) 
+login_button.place(x = 200, y = 150) 
 # Tao ra thanh cong cu giup nguoi dung moi tao tai khoan 
 signup_label = tk.Label(window_login, text="Create new account !!")
 signup_button = tk.Button(window_login, text = "click here" , command= Add_infor) 
@@ -90,4 +89,14 @@ signup_button = tk.Button(window_login, text = "click here" , command= Add_infor
 signup_label.place(x = 100, y = 200) 
 signup_button.place(x = 220, y = 200) 
 # vong lap trang web 
-window_login.mainloop( )
+window_login.mainloop()
+
+root = tk.Tk()
+root.geometry( "350x500") 
+root.title("Chat Bot")
+
+
+massege = tk.Entry(root) 
+massege.place(x = 50, y = 450)
+
+root.mainloop()
